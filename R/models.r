f83fffd0bbf0d0d5e4d578db7816587201d13a7b
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-04-17 14:14:21 -0300

    Melhora doc de EGO

--- R/models.r
+++ R/models.r
@@ -1,44 +1,46 @@
 
-#' Modelos Singleshot
+#' Modelo Singleshot
 #' 
 #' Estimadores de modelos singleshot com variados conjuntos de regressores
 #' 
-#' O argumento \code{params} pode ser uma lista contendo os seguintes elementos, divididos em
+#' @details 
+#' 
+#' ## `model_params`
+#' 
+#' O argumento `params` pode ser uma lista contendo os seguintes elementos, divididos em
 #' dois grupos. O primeiro grupo diz respeito a arquitetura do modelo, comecando com os tamanhos de
 #' sequencias para encoding
 #' 
-#' \enumerate{
-#' \item{L_carga}
-#' \item{L_mmgd}
-#' \item{L_headindex}
-#' \item{L_temperatura}
-#' }
+#' * `L_carga`
+#' * `L_mmgd`
+#' * `L_headindex`
+#' * `L_temperatura`
 #' 
 #' Deve ser notado que os tres primeiros dizem respeito a valores observados, portanto tamanhos de 
 #' sequencias recentes a serem incluidas no modelo. A sequencia de temperatura combinara, se
 #' necessario, dados observados e previstos. Em seguida estao
-#'                         
-#' \enumerate{
-#' \item{hora_inicio}{ultima hora do dia disponivel quando da previsao}
-#' \item{trend}{booleano, se deve ser modelada tendencia linear no tempo}
-#' \item{roll_temp}{booleano, se temperatura deve ser uma sequencia caminhando no tempo ou sempre a 
-#'     mesma contemplando todo o trajeto ate o final do dia seguinte}
-#' }
 #' 
-#' Os demais elementos de \code{params} podem corresponder a qualquer um dos argumentos passados no
-#' argumento \code{params} de \code{\link[lightgbm]{lgb.train}} ou \code{\link[lightgbm]{lgb.Dataset}}
+#' * `hora_inicio`: ultima hora do dia disponivel quando da previsao
+#' * `trend`: booleano, se deve ser modelada tendencia linear no tempo
+#' * `roll_temp`: booleano, se temperatura deve ser uma sequencia caminhando no tempo ou sempre a 
+#'     mesma contemplando todo o trajeto ate o final do dia seguinte
 #' 
-#' * S: 32 valores de carga e 64 de temperatura
-#' * M: padrao, 64 valores de carga e 128 de temperatura
-#' * L: 64 valores de carga, 64 valores de mmgd e 128 de temperatura
-#' * XL: 64 valores de carga, 64 valores de mmgd, 64 valores de heatindex e 128 de temperatura
+#' Os demais elementos de `params` podem corresponder a qualquer um dos argumentos passados no
+#' argumento `params` de [lightgbm::lgb.train()] ou [lightgbm::lgb.Dataset()]
+#' 
+#' Para facilitacao de testes mais padronizados, existe uma funcao [default_model_params_ego()] que
+#' retorna uma lista completa de parametros.
+#' 
+#' ## `test_params`
 #' 
 #' @param carga,temp_obs,temp_prev,feriados data.tables de dados para geracao dos regressores
 #' @param model_params lista de parametros do modelo. Veja Detalhes
+#' @param test_params lista configurando o tipo e parametros de teste do modelo. Veja Detalhes
 
 EGO <- function(
     carga, temp_obs, temp_prev, feriados,
-    params = default_params_ego("M")
+    model_params = default_model_params_ego("M"),
+    test_params = default_test_params_ego()
 ) {
     NA
 }
